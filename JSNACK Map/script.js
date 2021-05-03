f95e64f515608cbfce3c5bedac565b723883c317
NicolaDiSante <nickymacha90@gmail.com>
2021-05-03 14:47:24 +0200

    Rimossi e spostati su Jsnack es6

--- JSNACK Map/script.js
+++
@@ -1,6 +0,0 @@
-const arrNames = ["pippo", "Pluto", "PAPERINO", "poLDO"];
-
-for(let name of arrNames){
-  name = name.charAt(0).toUpperCase() + name.substr(1).toLowerCase();
-  console.log(name);
-}
--- Jsnack array, oggetti, map, interpolazione delle stringhe/script.js
+++
@@ -1,6 +0,0 @@
-const persone = [
-	{ nome: 'Mario', cognome: 'Rossi', eta: 60 },
-	{ nome: 'Luigi', cognome: 'Verdi', eta: 12 },
-	{ nome: 'Silvia', cognome: 'Neri', eta: 25 },
-]
-
